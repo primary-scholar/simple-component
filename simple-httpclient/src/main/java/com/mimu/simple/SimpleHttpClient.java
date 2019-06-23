1a73bfb8569e40aea0322e107a72545057691ad5
mimu <mimu@mimu-mac.local>
2019-06-23 13:00:26 +0800

    fulfill simple-httpclient model

--- simple-httpclient/src/main/java/com/mimu/simple/SimpleHttpClient.java
+++ simple-httpclient/src/main/java/com/mimu/simple/SimpleHttpClient.java
@@ -14,6 +14,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -22,10 +23,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class SimpleHttpClient {
     private static final Logger logger = LoggerFactory.getLogger(SimpleHttpClient.class);
@@ -39,34 +38,6 @@ public class SimpleHttpClient {
 
     static {
         initHttpClient();
-    }
-
-    public static CloseableHttpClient initHttpClient() {
-        if (httpClient == null) {
-            synchronized (lock) {
-                if (httpClient == null) {
-                    PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
-                    poolingHttpClientConnectionManager.setMaxTotal(500);
-                    poolingHttpClientConnectionManager.setDefaultMaxPerRoute(50);
-                    httpClient = HttpClients.custom().setConnectionManager(poolingHttpClientConnectionManager).build();
-                    if (logger.isDebugEnabled()) {
-                        logger.debug("initHttpClient={}", httpClient.hashCode());
-                    }
-                }
-            }
-        }
-        return httpClient;
-    }
-
-    public static RequestConfig getRequestConfig(int connectionTime, int readTime) {
-        if (connectionTimeOut > 0) {
-            connectionTimeOut = connectionTime;
-        }
-        if (readTimeOut > 0) {
-            readTimeOut = readTime;
-        }
-        return RequestConfig.custom().setConnectTimeout(connectionTimeOut)
-                .setSocketTimeout(readTimeOut).build();
     }
 
     public static String get(String url) {
@@ -216,5 +187,57 @@ public class SimpleHttpClient {
 
             }
         }
+    }
+
+    private static void initHttpClient() {
+        if (httpClient == null) {
+            synchronized (lock) {
+                if (httpClient == null) {
+                    PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+                    connectionManager.setMaxTotal(500);
+                    connectionManager.setDefaultMaxPerRoute(50);
+                    httpClient = HttpClients.custom().setConnectionManager(connectionManager).setRetryHandler(new StandardHttpRequestRetryHandler(1, false)).build();
+                    /**
+                     * 服务端 关闭连接 客户端无感知，HttpClient为了缓解这一问题，
+                     * 在某个连接使用前会检测这个连接是否过时，如果过时则连接失效，但是这种做法会为每个请求增加一定额外开销，
+                     * 因此有一个定时任务专门回收长时间不活动而被判定为失效的连接，可以某种程度上解决这个问题
+                     */
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            /**
+                             * 关闭失效链接，并已出链接池
+                             */
+                            connectionManager.closeExpiredConnections();
+                            /**
+                             * 关闭不活跃链接
+                             */
+                            connectionManager.closeIdleConnections(20, TimeUnit.SECONDS);
+                        }
+                    }, 0, 1000 * 10);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param connectionTime 链接建立时间
+     * @param readTime       请求翻出后等待对方相应时间
+     * @return
+     */
+    private static RequestConfig getRequestConfig(int connectionTime, int readTime) {
+        if (connectionTimeOut > 0) {
+            connectionTimeOut = connectionTime;
+        }
+        if (readTimeOut > 0) {
+            readTimeOut = readTime;
+        }
+        /**
+         * connectionRequestTimeOut 从链接池获取链接的 超时时间
+         */
+        int connectionRequestTimeOut = 3000;
+        return RequestConfig.custom().setConnectTimeout(connectionTimeOut).setConnectionRequestTimeout(connectionRequestTimeOut)
+                .setSocketTimeout(readTimeOut).build();
     }
 }
