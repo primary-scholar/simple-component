2e918729860a7eceb715d37ea256c4e1d885af34
mimu <mimu@mimu-mini.local>
2020-06-27 11:17:21 +0800

    format project

--- simple-httpserver/src/test/java/com/mimu/simple/AppConfig.java
+++ simple-httpserver/src/test/java/com/mimu/simple/AppConfig.java
@@ -1,7 +1,9 @@
 package com.mimu.simple;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses= SimpleHttpServerTest.class)
 public class AppConfig {
 }
