05804a96d2d5d218c8af402160e007a65f78ab59
mimu <mimu@mimu-mbp.local>
2020-06-28 20:08:09 +0800

    add simple-httpserver-skywalking-plugin

---
+++ simple-httpserver-skywalking-plugin/src/test/java/com/mimu/simple/server/plugin/AppTest.java
@@ -0,0 +1,20 @@
+package com.mimu.simple.server.plugin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
