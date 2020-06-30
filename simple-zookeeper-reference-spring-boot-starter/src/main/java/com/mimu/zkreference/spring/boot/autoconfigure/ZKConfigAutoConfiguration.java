c9c317df344fd7c788dd503bdadca8797db659a0
mimu <mimu@mimu-mbp.local>
2020-06-30 12:47:18 +0800

    modify project add simple-zookeeper-reference-spring-boot-starter

---
+++ simple-zookeeper-reference-spring-boot-starter/src/main/java/com/mimu/zkreference/spring/boot/autoconfigure/ZKConfigAutoConfiguration.java
@@ -0,0 +1,19 @@
+package com.mimu.zkreference.spring.boot.autoconfigure;
+
+import com.mimu.simple.zkreference.annotation.interceptor.ZKPropertyAspectSupport;
+import com.mimu.simple.zkreference.zkconfig.ZKConfigOperator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnBean(ZKPropertyAspectSupport.class)
+@EnableConfigurationProperties(ZKConfigProperties.class)
+public class ZKConfigAutoConfiguration {
+
+    @Bean
+    public ZKConfigOperator zkConfigResource(ZKConfigProperties zkConfigProperties) {
+        return new ZKConfigOperator(zkConfigProperties.getAddress(), zkConfigProperties.getPath());
+    }
+}
---
+++ simple-zookeeper-reference-spring-boot-starter/src/main/java/com/mimu/zkreference/spring/boot/autoconfigure/ZKConfigProperties.java
@@ -0,0 +1,27 @@
+package com.mimu.zkreference.spring.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(
+        prefix = "spring.zookeeper.center"
+)
+public class ZKConfigProperties {
+    private String address;
+    private String path;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
